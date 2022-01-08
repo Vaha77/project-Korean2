7f1b89451969cef558a3d627ce43e5b82e3f98c2
vaha77 <abdulvohid996@gmail.com>
2022-01-09 03:52:54 +0900

    newProject-kor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
 export const App = () => {
   return (
-    <div className="App">
-      <h1>App</h1>
-      <p>App page</p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>App</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 export default App;
