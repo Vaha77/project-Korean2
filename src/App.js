91d51709d83b5d78ec40edc2514bd4172be5b9b0
vaha77 <abdulvohid996@gmail.com>
2022-01-13 00:01:44 +0900

    Button

--- src/App.js
+++ src/App.js
@@ -5,16 +5,42 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
+import MyButton from "./Components/MyButton";
 export const App = () => {
+  // const env = process.env;
+  // env.PUBLIC_URL = env.PUBLIC_URL || "";
   return (
     <BrowserRouter>
       <div className="App">
         <h1>App</h1>
+        {/* <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} /> */}
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 틀릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 틀릭")}
+          type={"negative"}
+        />
+
+        <MyButton text={"버튼"} onClick={() => alert("버튼 틀릭")} />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 틀릭")}
+          type={"cscscscscs"}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
-          <Route path="/diary" element={<Diary />} />
+          <Route path="/diary/:id" element={<Diary />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 export const Edit = () => {
+  const [searchParams, setSearchPArams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id:", id);
+
   return (
     <div>
       <h1>Edit</h1>
